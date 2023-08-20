9cd1dd5289d6fb3a0cf2a0a11521ea9dd8288e8e
yash-tyagi-2003 <141833519+yash-tyagi-2003@users.noreply.github.com>
2023-08-20 23:29:02 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1,10 +1,32 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import main
+from flask_cors import CORS
+import requests
 
-app=Flask(__name__)
+app = Flask(__name__)
+CORS(app)
 
-@app.route("/")
-def home:
-    return "‹h1›ZeetFlask‹/h1›"
+@app.route('/')
+def hello_world():
+    return "Hello, World!"
 
-if __name__=="__main__":
-    app.run(debug=False, host="0.0.0.0")
+@app.route('/recommend', methods=['POST'])
+def recommend():
+    try:
+        data = request.get_json()
+        movie_title = data['movie_title']
+
+        similar_movies = main.find_similar(movie_title)
+
+        # Fetch movie details from the MovieDB API in the backend
+        movie_details = []
+        for movie_id in similar_movies:
+            response = requests.get(f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=cef4366e188e22031754054cd680ec34&language=en-US")
+            movie_details.append(response.json())
+
+        return jsonify({'recommendations': movie_details})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=5000)
