565fb85e0b44acd62fc788df499f41198ebc4f89
yash-tyagi-2003 <141833519+yash-tyagi-2003@users.noreply.github.com>
2023-08-20 23:14:58 +0530

    Update api.py

--- api.py
+++ api.py
@@ -4,10 +4,29 @@ from flask_cors import CORS
 import requests
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello_world():
     return "Hello, World!"
+
+@app.route('/recommend', methods=['POST'])
+def recommend():
+    try:
+        data = request.get_json()
+        movie_title = data['movie_title']
+
+        similar_movies = main.find_similar(movie_title)
+
+        # Fetch movie details from the MovieDB API in the backend
+        movie_details = []
+        for movie_id in similar_movies:
+            response = requests.get(f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=cef4366e188e22031754054cd680ec34&language=en-US")
+            movie_details.append(response.json())
+
+        return jsonify({'recommendations': movie_details})
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000)
